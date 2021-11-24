d9181dc48f47980003b5030737886a6165942105
BohdanDehodiuk <86683356+BohdanDehodiuk@users.noreply.github.com>
2021-11-24 21:59:43 +0200

    Finish HW#8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,65 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
+  }, 1000),
+);
+
+const sevedDataTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+player
+  .setCurrentTime(sevedDataTime)
+  .then(seconds => {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(error => {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+player.on('play', () => {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(title => {
+  console.log('title:', title);
+});
+
+player
+  .setColor('#59EF0F')
+  .then(color => {
+    // color was successfully set
+  })
+  .catch(error => {
+    switch (error.name) {
+      case 'ContrastError':
+        // the color was set, but the contrast is outside of the acceptable
+        // range
+        break;
+
+      case 'TypeError':
+        // the string was not a valid hex or rgb color
+        break;
+
+      case 'EmbedSettingsError':
+        // the owner of the video has chosen to use a specific color
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+var throttle = require('lodash.throttle');
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+form.addEventListener('input', throttle(onFormInput, 500));
+
+pushToTextarea();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(formData);
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function pushToTextarea() {
+  const savedMassage = localStorage.getItem(STORAGE_KEY);
+  const parsedStorage = JSON.parse(savedMassage);
+  if (savedMassage) {
+    form.email.value = parsedStorage.email;
+    form.message.value = parsedStorage.message;
+  }
+}
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
